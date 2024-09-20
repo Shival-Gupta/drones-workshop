2f8837da6afcbfd808511b4a9e85f39c6c143d9d
Shival Gupta <sgupta.5545@gmail.com>
2024-09-20 09:09:13 +0530

    Update

--- README.md
+++ README.md
@@ -1,1 +1,99 @@
-# drones-workshop
+# Automated Drone Simulation Setup
+
+This guide will help you set up a complete simulation environment for drones using PX4, Ardupilot, Gazebo, MAVROS, and IQ Sim with minimal human intervention.
+
+## Requirements
+
+1. Ubuntu 20.04 LTS (for ROS Noetic compatibility)
+2. Internet connection for downloading required packages.
+
+## Steps to Run the Setup Script
+
+### 1. Clone the repository (if not done already)
+You need to get the `setup.sh` script. You can clone it using:
+```bash
+git clone https://github.com/YourRepo/YourProject.git
+cd YourProject
+```
+
+### 2. Run the `setup.sh` Script
+The script will automate the installation and setup process for ROS, PX4, Ardupilot, Gazebo, MAVROS, and IQ Sim.
+
+Run the script:
+```bash
+chmod +x setup.sh
+./setup.sh
+```
+
+### 3. Launch PX4 SITL with Gazebo
+After the installation, you can run PX4 SITL (Software In The Loop) with Gazebo using the following commands:
+```bash
+cd ~/PX4-Autopilot
+make px4_sitl_default gazebo
+```
+
+### 4. Launch Ardupilot SITL with Gazebo
+To run Ardupilot with Gazebo:
+1. Launch Gazebo with the Ardupilot-Gazebo plugin:
+   ```bash
+   gazebo --verbose ~/ardupilot_gazebo/worlds/iris_arducopter_runway.world
+   ```
+2. In another terminal, launch Ardupilot SITL:
+   ```bash
+   cd ~/ardupilot/ArduCopter
+   sim_vehicle.py -v ArduCopter -f gazebo-iris --console
+   ```
+
+### 5. Running MAVROS and IQ Sim
+To launch IQ Sim and get telemetry data from the drone:
+
+1. In one terminal, launch the runway simulation:
+   ```bash
+   roslaunch iq_sim runway.launch
+   ```
+
+2. In another terminal, run Ardupilot SITL:
+   ```bash
+   cd ~
+   ./startsitl.sh
+   ```
+
+3. Open QGroundControl:
+   ```bash
+   ./QGroundControl.AppImage
+   ```
+
+4. Plan a mission in QGroundControl, and execute it.
+
+### 6. Using MAVROS and Checking Telemetry Data
+To monitor telemetry data:
+
+1. Start MAVROS:
+   ```bash
+   roslaunch iq_sim apm.launch
+   ```
+
+2. View telemetry data from FCU:
+   ```bash
+   rostopic echo /mavros/global_position/local
+   ```
+
+3. You can interact with MAVProxy to control the drone:
+   ```bash
+   mode guided
+   arm throttle
+   takeoff 10
+   ```
+
+## Troubleshooting
+- If Gazebo doesn't launch or fails to connect with the simulation, ensure you have installed the Gazebo 11 version using the `setup.sh` script.
+- Verify environment variables are set correctly by running:
+  ```bash
+  source ~/.bashrc
+  ```
+
+## Additional Information
+- PX4 Autopilot: [PX4 Official Site](https://px4.io/)
+- ArduPilot: [ArduPilot Official Site](https://ardupilot.org/)
+- QGroundControl: [QGroundControl Download](https://qgroundcontrol.com/)
+
--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# 2. Update apt repositories and upgrade packages
-sudo add-apt-repository universe multiverse restricted && \
-sudo apt update && \
-sudo apt upgrade -y
+# 1. Set WSL default version and install Ubuntu (if running on WSL)
+# Uncomment the following two lines for WSL setup if required:
+# wsl --set-default-version 2
+# wsl --install -d Ubuntu-20.04
 
-# 3. Install core development tools and libraries (combined into single command)
+# 2. Update apt repositories and upgrade packages
+sudo add-apt-repository universe multiverse restricted -y && \
+sudo apt update && sudo apt upgrade -y
+
+# 3. Install core development tools and libraries
 sudo apt install -y git \
                     python3 python3-pip python3-dev \
                     nvidia-cuda-toolkit \
@@ -13,30 +17,86 @@ sudo apt install -y git \
                     libeigen3-dev libopencv-dev \
                     libyaml-cpp-dev python3-yaml \
                     libboost-all-dev libcurl4-openssl-dev \
-                    libxml2-dev libbz2-dev
+                    libxml2-dev libbz2-dev \
+                    curl python3-rosdep python3-rosinstall \
+                    python3-rosinstall-generator python3-wstool \
+                    ros-noetic-desktop-full
 
-# 4. Install ROS Noetic
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' && \
-sudo apt install -y curl && \
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add - && \
-sudo apt update && \
-sudo apt install -y ros-noetic-desktop-full 
-
-# 5. Source ROS setup, install tools, and initialize rosdep (combined)
+# 4. Set up ROS Noetic and environment variables
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
-
-# 6. Add environment variables to .bashrc
 echo "export GAZEBO_IP=127.0.0.1" >> ~/.bashrc
 echo "export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0" >> ~/.bashrc
 echo "export LIBGL_ALWAYS_INDIRECT=0" >> ~/.bashrc
+source ~/.bashrc
 
-source ~/.bashrc && \
-sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential && \
 sudo rosdep init && \
 rosdep update
 
-# 7. Install PX4 SITL 
-git clone https://github.com/PX4/PX4-Autopilot.git && \
-cd PX4-Autopilot && \
-bash ./Tools/setup/ubuntu.sh # Consider adding `-y` or other flags if the script supports it
-make px4_sitl_default gazebo # or make px4_sitl_default gazebo [model_name]
+# 5. Install PX4 SITL
+cd ~
+git clone https://github.com/PX4/PX4-Autopilot.git
+cd PX4-Autopilot
+bash ./Tools/setup/ubuntu.sh -y
+make px4_sitl_default gazebo
+
+# 6. Install Ardupilot SITL and dependencies
+cd ~
+git clone https://github.com/ArduPilot/ardupilot.git
+cd ardupilot
+git checkout Copter-4.0.4
+git submodule update --init --recursive || git config --global url.https://.insteadOf git://
+Tools/environment_install/install-prereqs-ubuntu.sh -y
+source ~/.profile
+cd ~/ardupilot/ArduCopter
+sim_vehicle.py -w # Initialize SITL with default configuration
+
+# 7. Setup Gazebo and Ardupilot-Gazebo Plugin
+sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" > /etc/apt/sources.list.d/gazebo-stable.list'
+wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
+sudo apt update
+sudo apt-get install -y gazebo11 libgazebo11-dev
+
+cd ~
+git clone https://github.com/khancyr/ardupilot_gazebo.git
+cd ardupilot_gazebo
+mkdir build && cd build
+cmake ..
+make -j4
+sudo make install
+echo 'source /usr/share/gazebo/setup.sh' >> ~/.bashrc
+echo 'export GAZEBO_MODEL_PATH=~/ardupilot_gazebo/models' >> ~/.bashrc
+source ~/.bashrc
+
+# 8. Install MAVROS, MAVLink, and IQ Sim
+cd ~
+mkdir -p ~/catkin_ws1/src
+cd ~/catkin_ws1
+catkin init
+wstool init ~/catkin_ws1/src
+rosinstall_generator --upstream mavros | tee /tmp/mavros.rosinstall
+rosinstall_generator mavlink | tee -a /tmp/mavros.rosinstall
+wstool merge -t src /tmp/mavros.rosinstall
+wstool update -t src
+rosdep install --from-paths src --ignore-src --rosdistro `echo $ROS_DISTRO` -y
+catkin build
+echo "source ~/catkin_ws1/devel/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo ~/catkin_ws1/src/mavros/mavros/scripts/install_geographiclib_datasets.sh
+
+# 9. Clone and build IQ Sim and GNC
+cd ~/catkin_ws1/src
+git clone https://github.com/Intelligent-Quads/iq_sim.git
+git clone https://github.com/Intelligent-Quads/iq_gnc.git
+catkin build
+echo "GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:$HOME/catkin_ws1/src/iq_sim/models" >> ~/.bashrc
+source ~/.bashrc
+
+# 10. Install QGroundControl
+cd ~
+sudo usermod -a -G dialout $USER
+sudo apt-get remove modemmanager -y
+wget https://s3-us-west-2.amazonaws.com/qgroundcontrol/latest/QGroundControl.AppImage
+chmod +x ./QGroundControl.AppImage
+
+echo "Setup complete. To run QGroundControl, execute ./QGroundControl.AppImage."
+echo "To run ArduCopter SITL with Gazebo, run the commands in the README."
